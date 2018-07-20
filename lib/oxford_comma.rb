cb1badb24b518b779289be1631912ab48e759f34
Malcome Fein <mfein90@gmail.com>
2018-07-20 08:57:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
+# require 'pry'
 def oxford_comma(array)
-
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+  # binding.pry 
+    array[-1].prepend "and "
+	array.join(", ")
+  end
 end
